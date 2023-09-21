1ea456aaa156ce038113cedf8ecd7af2381b536f
Isabella-Kim <jeonghyeon00777@gmail.com>
2023-09-22 02:53:10 +0900

    3rd commit: 홈 코딩중

--- src/App.js
+++ src/App.js
@@ -19,15 +19,80 @@ import Blog from "./Components/Blog";
 import Contact from "./Components/Contact";
 
 function App() {
+  // 섹션 변경을 감지
+  const [activeSection, setActiveSection] = useState(null);
+
+  // useRef로 각 섹션을 구분
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const servicesRef = useRef(null);
+  const portfolioRef = useRef(null);
+  const blogRef = useRef(null);
+  const contactRef = useRef(null);
+
+  // 각 섹션 시작점 구하기
+  useEffect(() => {
+    const checkSection = () => {
+      const sections = [
+        { ref: homeRef, id: "home" },
+        { ref: aboutRef, id: "about" },
+        { ref: servicesRef, id: "services" },
+        { ref: portfolioRef, id: "portfolio" },
+        { ref: blogRef, id: "blog" },
+        { ref: contactRef, id: "contact" },
+      ];
+
+      for (const { ref, id } of sections) {
+        if (ref && ref.current) {
+          const sectionTop = ref.current.offsetTop;
+          const sectionBottom = sectionTop + ref.current.clientHeight;
+          const scrollY = window.scrollY;
+
+          if (scrollY >= sectionTop && scrollY < sectionBottom) {
+            setActiveSection(id);
+            console.log("현재 섹션:", id); // 현재 섹션을 콘솔에 출력
+            break;
+          }
+        }
+      }
+    };
+
+    // 스크롤 이벤트 리스너 등록
+    window.addEventListener("scroll", checkSection);
+
+    // 초기 섹션 체크
+    checkSection();
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener("scroll", checkSection);
+    };
+  }, []);
+
+  const chgSec = () => {};
   return (
     <div className="App">
       <Header />
-      <Home />
-      <About />
-      <Services />
-      <Portfolio />
-      <Blog />
-      <Contact />
+      <>
+        <div ref={homeRef}>
+          <Home />
+        </div>
+        <div ref={aboutRef}>
+          <About />
+        </div>
+        <div ref={servicesRef}>
+          <Services />
+        </div>
+        <div ref={portfolioRef}>
+          <Portfolio />
+        </div>
+        <div ref={blogRef}>
+          <Blog />
+        </div>
+        <div ref={contactRef}>
+          <Contact />
+        </div>
+      </>
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
-  return <div className="Home">Home</div>;
+  const [currentNameIndex, setCurrentNameIndex] = useState(0);
+  const names = ["JEONHGYEON", "ISABELLA"];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentNameIndex((prevIndex) => (prevIndex === 0 ? 1 : 0));
+    }, 2000); // 2초마다 이름 변경
+
+    return () => {
+      clearInterval(intervalId); // 컴포넌트가 언마운트되면 interval 정리
+    };
+  }, []);
+  return (
+    <div className="Home">
+      <div className="wrapHomeInfo">
+        <div className="wrapHomeName">
+          <p className="name">MY NAME IS</p>
+          <p className="name">KIM</p>
+          <div className="chgWord">
+            <p className="name chgName">{names[currentNameIndex]}</p>
+          </div>
+          <p>
+            <b>Front-end Developer </b>from RoK
+          </p>
+        </div>
+        <div className="toContact">
+          <button>Let Us Make the Great Work</button>
+        </div>
+        <div className="mail">
+          <div className="wrapMail">
+            <div className="naverMail">
+              <FontAwesomeIcon icon={faEnvelope} className="faHome" />
+              <p>jack6512@naver.com</p>
+            </div>
+            <div className="gMail">
+              <FontAwesomeIcon icon={faEnvelopeOpen} className="faHome" />
+              <p>jeonghyeon00777@gmail.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="wrapHomeImg">
+        <img
+          src={`${process.env.PUBLIC_URL}/images/Home/HomeProfile.png`}
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
