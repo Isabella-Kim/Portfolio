7da7c463cd4d7fb516bc49c515fff307a6a91152
Isabella-Kim <jeonghyeon00777@gmail.com>
2023-09-23 03:53:24 +0900

    5th commit: 어바웃 완료, 컨택트 완료(메일전송가능)

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,122 +1,143 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
-  const accData = [
-    {
-      title: "EDUCATION",
-      items: [
-        {
-          type: "img",
-          content: `${process.env.PUBLIC_URL}/images/About/BookOpen.png`,
-        },
-        {
-          type: "content",
-          content: [
-            "2023.04 - 2023.10 그린컴퓨터아트아카데미 리엑트(React), 리엑트네이티브 EXPO 활용 프론트앤드 개발자 양성과정",
-          ],
-        },
-        {
-          type: "img",
-          content: `${process.env.PUBLIC_URL}/images/About/BookOpenText.png`,
-        },
-        {
-          type: "content",
-          content: ["2023 - 한국방송통신대학교 컴퓨터과학과 재학"],
-        },
-        {
-          type: "img",
-          content: `${process.env.PUBLIC_URL}/images/About/Student.png`,
-        },
-        {
-          type: "content",
-          content: ["2017 - 2022 충남대학교 정치외교학과 · 무역학과 졸업"],
-        },
-      ],
-    },
-    {
-      title: "EXPERIENCE",
-      items: [], // 비어있어도 괜찮음
-      content: [
-        "2022 - 2023 현대경제연구원 유라시아청년아카데미 / (2022) 제4기 교육생 · (2023) 제3기 알럼나이 서포터즈 ",
-        "2022.01 (주)오픈프레스 / 신규론칭 ‘펫봄’ 애플리케이션 마케팅 제안서 제출",
-        "2017.02 - 2020.12 대전예술의전당 / 하우스 어셔로 근무",
-      ],
-    },
-  ];
+  // 두 개의 아코디언 아이템 상태 변수 추가
+  const [educationOpen, setEducationOpen] = useState(true);
+  const [experienceOpen, setExperienceOpen] = useState(false);
+
+  // Education 아코디언 열기 함수
+  const openEducation = () => {
+    setEducationOpen(true);
+    setExperienceOpen(false); // Experience 아코디언 닫기
+  };
+
+  // Experience 아코디언 열기 함수
+  const openExperience = () => {
+    setExperienceOpen(true);
+    setEducationOpen(false); // Education 아코디언 닫기
+  };
 
   return (
     <div className="About">
-      <div className="aboutTitle commonTitle">
-        <p className="subTitle">Mucho Gusto!</p>
-        <h1 className="mainTitle">WHO AM I?</h1>
-      </div>
-      <div className="aboutProfile">
-        <img
-          src={`${process.env.PUBLIC_URL}/images/About/AboutProfile.png`}
-          alt=""
-        />
-        <div className="aboutName">
-          <p>김정현</p>
-          <p>KIM JEONGHYEON</p>
+      <div className="inner">
+        <div className="aboutTitle commonTitle">
+          <p className="subTitle">Mucho Gusto!</p>
+          <h1 className="mainTitle">WHO AM I?</h1>
         </div>
-      </div>
-      <div className="aboutDescription">
-        <div className="aboutMe">
-          <p>[ THIS IS ME ]</p>
-          <p>
-            “Diligent, Responsible, and Endeavoring.”
-            <br />
-            업무나 프로젝트를 수행할 때, 항상 최선을 다하고 주어진 일을 완수하기
-            위해 노력합니다. 동시에, 도전을 두려워하지 않으며 어려움 앞에서도
-            쉽게 물러서지 않습니다. 이러한 강점을 바탕으로 작업 수행에 가치를
-            더하고 끊임없이 성장하려는 개발자가 되겠습니다.
-          </p>
-        </div>
-        <div className="aboutAccordion">
-          {accData.map((item, index) => (
-            <div key={index} className="accItem">
-              <h2>
-                <img
-                  src={`${process.env.PUBLIC_URL}/images/About/twincle.png`}
-                  alt=""
-                />
-                {item.title}
-                <img
-                  src={`${process.env.PUBLIC_URL}/images/About/sprite.png`}
-                  alt=""
-                />
-              </h2>
-              <ul>
-                {item.items.map((subItem, subIndex) => (
-                  <li key={subIndex}>
-                    {subItem.type === "img" ? (
-                      <img src={subItem.content} alt={`Image ${subIndex}`} />
-                    ) : (
-                      subItem.content.map((content, contentIndex) => (
-                        <p key={contentIndex}>{content}</p>
-                      ))
-                    )}
-                  </li>
-                ))}
-              </ul>
-              {item.title === "EXPERIENCE" && (
-                <ul>
-                  {item.content.map((content, contentIndex) => (
-                    <li key={contentIndex}>
-                      <img
-                        src={`${process.env.PUBLIC_URL}/images/About/star.png`}
-                        alt=""
-                      />
-                      <p>{content}</p>
-                    </li>
-                  ))}
-                </ul>
-              )}
+        <div className="aboutMain">
+          <div className="aboutProfile">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/About/AboutProfile.png`}
+              alt=""
+            />
+            <div className="aboutName">
+              <p>김정현</p>
+              <p>KIM JEONGHYEON</p>
             </div>
-          ))}
-        </div>
-        <div className="aboutMotto">
-          <h2>"Nothing Is Impossible"</h2>
+          </div>
+          <div className="aboutDescription">
+            <div className="aboutMe">
+              <p>[ THIS IS ME ]</p>
+              <p>
+                <i>“Diligent, Responsible, and Endeavoring.”</i>
+                <br />
+                업무나 프로젝트를 수행할 때, 항상 최선을 다하고 주어진 일을
+                완수하기 위해 노력합니다. 동시에, 도전을 두려워하지 않으며
+                어려움 앞에서도 쉽게 물러서지 않습니다. 이러한 강점을 바탕으로
+                작업 수행에 가치를 더하고 끊임없이 성장하려는 개발자가
+                되겠습니다.
+              </p>
+            </div>
+            <div className="aboutAccordion">
+              <div className="education accordion">
+                <div className="accTitle" onClick={openEducation}>
+                  <div className="accTitleLeft">
+                    <img
+                      src={`${process.env.PUBLIC_URL}/images/About/twincle.png`}
+                      alt=""
+                    />
+                    <Link>
+                      <h2>EDUCATION</h2>
+                    </Link>
+                  </div>
+                  <span className={educationOpen ? "eyeOpen" : ""}></span>
+                </div>
+                <div className={`accContent ${educationOpen ? "open" : ""}`}>
+                  <div className="accComponent">
+                    <img
+                      src={`${process.env.PUBLIC_URL}/images/About/BookOpen.png`}
+                      alt=""
+                    />
+                    <p>
+                      2023.04 - 2023.10 그린컴퓨터아트아카데미 리엑트(React),
+                      리엑트네이티브 EXPO 활용 프론트앤드 개발자 양성과정
+                    </p>
+                  </div>
+                  <div className="accComponent">
+                    <img
+                      src={`${process.env.PUBLIC_URL}/images/About/BookOpenText.png`}
+                      alt=""
+                    />
+                    <p>2023 - 한국방송통신대학교 컴퓨터과학과 재학</p>
+                  </div>
+                  <div className="accComponent">
+                    <img
+                      src={`${process.env.PUBLIC_URL}/images/About/Student.png`}
+                      alt=""
+                    />
+                    <p>2017 - 2022 충남대학교 정치외교학과 · 무역학과 졸업</p>
+                  </div>
+                </div>
+              </div>
+              <div className="experience accordion">
+                <div className="accTitle" onClick={openExperience}>
+                  <div className="accTitleLeft">
+                    <img
+                      src={`${process.env.PUBLIC_URL}/images/About/twincle.png`}
+                      alt=""
+                    />
+                    <Link>
+                      <h2>EXPERIENCE</h2>
+                    </Link>
+                  </div>
+                  <span className={experienceOpen ? "eyeOpen" : ""}></span>
+                </div>
+                <div className={`accContent ${experienceOpen ? "open" : ""}`}>
+                  <div className="accComponent">
+                    <img
+                      src={`${process.env.PUBLIC_URL}/images/About/star.png`}
+                      alt=""
+                    />
+                    <p>
+                      2022 - 2023 현대경제연구원 유라시아청년아카데미 / (2022)
+                      제4기 교육생 · (2023) 제3기 알럼나이 서포터즈
+                    </p>
+                  </div>
+                  <div className="accComponent">
+                    <img
+                      src={`${process.env.PUBLIC_URL}/images/About/star.png`}
+                      alt=""
+                    />
+                    <p>
+                      2022.01 (주)오픈프레스 / 신규론칭 ‘펫봄’ 애플리케이션
+                      마케팅 제안서 제출
+                    </p>
+                  </div>
+                  <div className="accComponent">
+                    <img
+                      src={`${process.env.PUBLIC_URL}/images/About/star.png`}
+                      alt=""
+                    />
+                    <p>2017.02 - 2020.12 대전예술의전당 / 하우스 어셔로 근무</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="aboutMotto">
+              <h2>"Nothing Is Impossible"</h2>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,99 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  return <div className="Contact">Contact</div>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_wtrm1bo",
+        "template_o40fl2p",
+        form.current,
+        "TSa7Gh8YEGMU1ELrs"
+      )
+      .then(
+        (result) => {
+          alert("성공적으로 이메일이 전송되었습니다.");
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+          alert("이메일이 전송이 실패되었습니다.");
+        }
+      );
+  };
+
+  return (
+    <div className="Contact">
+      <div className="inner">
+        <div className="contactTitle commonTitle">
+          <div className="title">
+            <p className="subTitle">Work With Me?</p>
+            <h1 className="mainTitle">CONTACT</h1>
+          </div>
+          <p>If You Need My Services Or Want Me To Join, Lets Team Up!</p>
+        </div>
+        <form ref={form} className="sendMail">
+          <div className="generalInfo">
+            <div className="from_name">
+              <label htmlFor="Name">Name</label>
+              <input
+                type="text"
+                name="from_name"
+                placeholder="Enter your name"
+                required
+              />
+            </div>
+            <div className="phone_number">
+              <label htmlFor="PhoneNumber">Phone Number</label>
+              <input
+                type="number"
+                name="phone_number"
+                placeholder="Enter your phone number"
+                required
+              />
+            </div>
+          </div>
+          <div className="your_email">
+            <label htmlFor="YourEmail">Your Email</label>
+            <input
+              type="email"
+              name="your_email"
+              placeholder="Enter your email address"
+              required
+            />
+          </div>
+          <div className="subject">
+            <label htmlFor="Subject">Subject</label>
+            <input
+              type="text"
+              name="subject"
+              placeholder="Please enter a subject"
+              required
+            />
+          </div>
+          <div className="message">
+            <label htmlFor="Messages">Your Message</label>
+            <input
+              type="text"
+              name="message"
+              placeholder="Enter your message"
+              required
+            />
+          </div>
+          <button type="submit" className="btn" onClick={sendEmail}>
+            Send Message
+          </button>
+        </form>
+        <p className="copyright">
+          Copyright 2023. Kim Jeonghyeon. All right reserved.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
