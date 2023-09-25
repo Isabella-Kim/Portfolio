4aa1176c96e515ba32354f977495b62fb09cad8f
Isabella-Kim <jeonghyeon00777@gmail.com>
2023-09-25 16:25:28 +0900

    7th commit : 블로그 진행중

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,7 +1,73 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { postings } from "./BlogData/Data";
+import { Swiper, SwiperSlide } from "swiper/react"; // basic
+import SwiperCore, { Navigation, Pagination } from "swiper";
+import "swiper/css"; //basic
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Blog = () => {
-  return <div className="Blog">Blog</div>;
+  return (
+    <div className="Blog">
+      <div className="blogTitle commonTitle">
+        <div>
+          <p className="subTitle">My Everyday Writing</p>
+          <h1 className="mainTitle">BLOG POSTINGS</h1>
+        </div>
+        <button
+          onClick={() =>
+            window.open("https://m.blog.naver.com/jack6512", "_blank")
+          }
+        >
+          VISIT MY BLOG
+        </button>
+      </div>
+      <div className="blogMain">
+        <img
+          src={`${process.env.PUBLIC_URL}/images/Blog/filmBottom.png`}
+          alt=""
+          className="film filmTop"
+        />
+        <div className="postingArea">
+          <div className="posting">
+            <Swiper
+              spaceBetween={50}
+              slidesPerView={3}
+              scrollbar={{ draggable: true }}
+              navigation
+              pagination={{ clickable: true }}
+              breakpoints={{
+                768: {
+                  slidesPerView: 7,
+                },
+              }}
+            >
+              <SwiperSlide>
+                {postings.map((positings) => (
+                  <div key={positings.id} className="posting">
+                    <img src={positings.img} alt="" />
+                    <h2>{positings.title}</h2>
+                  </div>
+                ))}
+              </SwiperSlide>
+            </Swiper>
+            {/* {postings.map((positings) => (
+              <div key={positings.id} className="posting">
+                <img src={positings.img} alt="" />
+                <h2>{positings.title}</h2>
+              </div>
+            ))} */}
+          </div>
+        </div>
+        <img
+          src={`${process.env.PUBLIC_URL}/images/Blog/filmbottom.png`}
+          alt=""
+          className="film filmBottom"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
---
+++ src/Components/BlogData/Data.js
@@ -0,0 +1,42 @@
+// data.js
+const postings = [
+  {
+    id: 1,
+    title: "[108일차]개인포폴작업",
+    img: "../../images/Services/html5.png",
+    url: "",
+  },
+  {
+    id: 2,
+    title:
+      "[107일차]Redux, useDispatch, useSelect를 활용하여 TMDB에서 API불러오는 영화 홈페이지",
+    img: "../../images/Services/html5.png",
+    url: "",
+  },
+  {
+    id: 3,
+    title: "[106일차]타슈 API Key의 배신과 CORS 해결을 위한 몸부림",
+    img: "../../images/Services/html5.png",
+    url: "",
+  },
+  {
+    id: 4,
+    title: "[105일차]Redux로 상태관리하기(2) + 포트폴리오 디자인 마무리",
+    img: "../../images/Services/html5.png",
+    url: "",
+  },
+  {
+    id: 5,
+    title: "[104일차]Redux로 상태관리하기",
+    img: "../../images/Services/html5.png",
+    url: "",
+  },
+  {
+    id: 6,
+    title: "[103일차]개인포트폴리오 사이트 디자인",
+    img: "../../images/Services/html5.png",
+    url: "",
+  },
+];
+
+export { postings };
